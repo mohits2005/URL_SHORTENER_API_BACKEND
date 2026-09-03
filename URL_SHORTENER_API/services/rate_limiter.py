fe84073f200a32fc5018d766d9e66c5628d7cf59
Mohit Sharma <mohitsharma55562005@gmail.com>
2026-09-03 20:30:05 +0530

    Update rate_limiter.py

--- URL_SHORTENER_API/services/rate_limiter.py
+++ URL_SHORTENER_API/services/rate_limiter.py
@@ -22,21 +22,16 @@ async def rate_limit(identifier: str, limit: int = 5, window: int = 60):
 
     key = f"rate_limit:{identifier}"
 
-    try:
-        current = await redis_client.incr(key)
+    current = await redis_client.incr(key)
 
-        if current == 1:
-            await redis_client.expire(key, window)
+    if current == 1:
+        await redis_client.expire(key, window)
 
-        if current > limit:
-            raise HTTPException(
-                status_code=429,
-                detail="Too many requests. Try again later."
-            )
-
-    except Exception:
-        # Redis not available → skip rate limiting
-        return
+    if current > limit:
+        raise HTTPException(
+            status_code=429,
+            detail="Too many requests. Try again later."
+        )
     
 #WITHOUT TRY EXCEPTION
 # async def get_rate_limit_status(identifier: str, limit: int = 10):
@@ -51,23 +46,23 @@ async def rate_limit(identifier: str, limit: int = 5, window: int = 60):
 #         "remaining": max(0, limit - count)
 #     }
 
-async def get_rate_limit_status(identifier: str, limit: int = 10):
+# async def get_rate_limit_status(identifier: str, limit: int = 10):
 
-    key = f"rate_limit:{identifier}"
+#     key = f"rate_limit:{identifier}"
 
-    if not redis_client:
-        return {
-            "used": 0,
-            "remaining": limit
-        }
+#     if not redis_client:
+#         return {
+#             "used": 0,
+#             "remaining": limit
+#         }
 
-    try:
-        count = await redis_client.get(key)
-        count = int(count) if count else 0
-    except Exception:
-        count = 0
+#     try:
+#         count = await redis_client.get(key)
+#         count = int(count) if count else 0
+#     except Exception:
+#         count = 0
 
-    return {
-        "used": count,
-        "remaining": max(0, limit - count)
-    }
+#     return {
+#         "used": count,
+#         "remaining": max(0, limit - count)
+#     }
