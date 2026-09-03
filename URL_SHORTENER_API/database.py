c8b9ab14271ce76687259e3a2a851b37291834f9
Mohit Sharma <mohitsharma55562005@gmail.com>
2026-09-03 20:28:32 +0530

    migrated db to supabase

--- URL_SHORTENER_API/database.py
+++ URL_SHORTENER_API/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-DATABASE_URL = "sqlite:///./url_shortener.db"
+DATABASE_URL = os.getenv("SUPABASE_DB_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
